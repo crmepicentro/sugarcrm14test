48a818175ee2c683196203bf99f00f59aaee17cf
Andrew <andres.g.alejandro@hotmail.com>
2025-08-29 15:50:07 -0500

    Aplicar .gitignore

--- custom/modules/DRI_Workflows/Ext/clients/base/layouts/subpanels/subpanels.ext.php
+++
@@ -1,9 +0,0 @@
-<?php
-// WARNING: The contents of this file are auto-generated.
-
-
-//auto-generated file DO NOT EDIT
-$viewdefs['DRI_Workflows']['base']['layout']['subpanels']['components'][]['override_subpanel_list_view'] = array (
-  'link' => 'opportunity_link',
-  'view' => 'subpanel-for-dri_workflows-opportunity_link',
-);
