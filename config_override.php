19dd421330f6d31d8822883741cc3a43da7ce6a6
Andrew <andres.g.alejandro@hotmail.com>
2025-08-29 15:46:48 -0500

    Dejar de seguir archivos ignorados

--- config_override.php
+++
@@ -1,9 +0,0 @@
-<?php
-/***CONFIGURATOR***/
-$sugar_config['default_language'] = 'es_ES';
-$sugar_config['enable_mobile_redirect'] = false;
-$sugar_config['js_custom_version'] = 18;
-$sugar_config['js_lang_version'] = 7;
-$sugar_config['disabled_languages'] = 'da_DK,ja_JP,es_LA,ca_ES,zh_CN,uk_UA,tr_TR,lv_LV,sv_SE,th_TH,sk_SK,ar_SA,sr_RS,fi_FI,et_EE,sq_AL,en_UK,pt_BR,zh_TW,ru_RU,ro_RO,pt_PT,pl_PL,nl_NL,nb_NO,ko_KR,lt_LT,it_it,hr_HR,he_IL,hu_HU,fr_FR,cs_CZ,el_EL,de_DE,bg_BG,en_us';
-$sugar_config['mobile']['enabled'] = false;
-/***CONFIGURATOR***/
